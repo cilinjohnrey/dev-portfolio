33e7eb05dda33e796ae34b0f8de37f556f9ab85a
John Rey Cilin <cilinjohnrey@gmail.com>
2024-07-06 19:14:58 +0800

    Implement about-me animation and text

--- src/components/About-me.vue
+++ src/components/About-me.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="about-me-container">
-    <div class="about-me-image">
+    <div class="about-me-image" v-tilt="{ max: 10, speed: 400}">
         <img src="/Laptop.png" alt="Laptop with code">
     </div>
     <div class="about-me-text">
         <h2>ABOUT ME</h2>
-        <p>💻 I'm a devoted software engineer with skills in both front-end and back-end web application development, driven by a passion using technology to solve real-world problems, make a positive impact on communities.</p>
-        <p>🚀My startup journey at hinulawanlab.com, Inc. exposed me to the dynamic world of entrepreneurship, help build Passafund, an innovative lending and borrowing marketplace.</p>
-        <p id="portfolio">🌏My ultimate goal is to collaborate with visionary organizations committed to using innovation to address societal challenges and make the world a better place to live.</p>
+        <p>💻 I'm a devoted web developer with skills in both front-end and back-end web application development, driven by a passion using technology to solve real-world problems, make a positive impact on communities.</p>
+        <p>🚀My startup journey at Passafund, exposed me to the dynamic world of entrepreneurship, an innovative lending and borrowing marketplace.</p>
+        <p id="portfolio">🌏My ultimate goal is to collaborate with visionary organizations committed to use innovation to address societal challenges and make the world a better place to live.</p>
     </div>
 </div>
 </template>
@@ -24,11 +24,13 @@
     align-items: center;
     justify-content: flex-start;
     width: 50%;
+    perspective: 1000px;
 }
 
 .about-me-image img {
     max-width: 100%;
     border-radius: 15px;
+    will-change: transform;
 }
 
 .about-me-text {
