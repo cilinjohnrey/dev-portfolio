547f06a2f7f2652d8f1bf08ce18333fe2fba8269
John Rey Cilin <cilinjohnrey@gmail.com>
2024-07-16 16:52:22 +0800

    Change the base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dev-portfolio/",
+  base: "/cilinjohnrey/",
   plugins: [
     vue(),
   ],
